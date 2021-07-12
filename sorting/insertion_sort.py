d1de8fc636f2fd917ba88296df8e25cf180eade8
Daniel Lee <dlee0528@hotmail.com>
2021-07-12 12:21:20 -0700

    implement merge sort

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -6,8 +6,8 @@ def insertion_sort(elements):
 
         j = i -1
 
-        while j >= 0 and anchor < elements[j]:
-            elements[j+1] = elements[j]
+        while j >= 0 and anchor < elements[j]: # iterate the left sorted array backwards
+            elements[j+1] = elements[j] 
             j = j - 1
         
         elements[j+1] = anchor
---
+++ sorting/merge_sort.py
@@ -0,0 +1,41 @@
+def merge_sort(a):
+    if len(a) > 1:
+        mid = len(a) // 2
+        left = a[:mid]
+        right = a[mid:]
+
+        merge_sort(left) # sort the first half
+        merge_sort(right) # sort the second half
+
+        i = j = k = 0
+
+        # copy data to temp arrays left and right
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                a[k] = left[i]
+                i += 1
+            else:
+                a[k] = right[j]
+                j += 1
+
+            k += 1
+
+        
+        # checking if any element was left
+        while i < len(left):
+            a[k] = left[i]
+            i += 1
+            k += 1
+
+        # checking if any element was left 
+        while j < len(right):
+            a[k] = right[j]
+            j += 1
+            k += 1
+
+
+if __name__ == "__main__":
+    a=[5,8,12,56,89, 100]
+    merge_sort(a)
+    print(a)
+    
