81511ddc081e69ca7315f4fc66e47b0b612a5f92
Daniel Lee <dlee0528@hotmail.com>
2021-07-12 16:04:09 -0700

    implement selection sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,14 +1,19 @@
-def selection_sort(elements):
-    size = len(elements)
+def selection_sort(arr):
+    size = len(arr)
 
-    for i in range(size-1):
-        for j in range(i, size):
-            if elements[i] <= elements[j]:
-                elements[i] = elements[i]
-
-        
+    if size < 1:
+        return 
     
+    for i in range(size-1):
+        min_index = i
 
+        for j in range(min_index+1, size):
+            if arr[j] <= arr[min_index]:
+                min_index = j
+            
+        if i != min_index:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            
 if __name__ == "__main__":
     elements = [5,9,2,1,67,34,88,34]
     selection_sort(elements)
